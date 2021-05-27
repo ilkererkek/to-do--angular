477e87d7751c0142c8da205fe4186905fe816406
ilkererkek <ilker.erke12@gmail.com>
2021-05-28 02:46:04 +0300

    form toggle added

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task'
+import { Subscription } from 'rxjs'
+import { UiService } from '../../services/ui.service'
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -11,10 +13,14 @@ export class AddTaskComponent implements OnInit {
   
   text: string = '';
   date: string = '';
-
-
   reminder: boolean = false;
-  constructor() { }
+
+  showAddTask: boolean = false;
+  subscription: Subscription | undefined ;
+  
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
@@ -36,5 +42,10 @@ export class AddTaskComponent implements OnInit {
     this.date = '';
     this.reminder = false;
   }
+  
+  toggleTaskForm(): void {
+    this.uiService.toggleAddTask()
+  }
+
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService} from '../../services/ui.service';
+import { Subscription} from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,15 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title : string =  'Todo App';
-  
+  showAddTask: boolean = false;
+  subscription: Subscription | undefined ;
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
 
   toggleTaskForm(): void {
-    console.log('toggle')
+    this.uiService.toggleAddTask()
   }
 
 }
